34601a19bce7299abd3c696c37630b5e9276d8ad
girish.d <girish.d@sonata-software.com>
2019-03-20 12:39:17 +0530

    removed on file

--- src/components/query-builder/query-entity.directive.ts
+++
@@ -1,7 +0,0 @@
-import { Directive, Input, TemplateRef } from '@angular/core';
-
-// tslint:disable-next-line:directive-selector
-@Directive({selector: '[queryEntity]'})
-export class QueryEntityDirective {
-  constructor(public template: TemplateRef<any>) {}
-}
